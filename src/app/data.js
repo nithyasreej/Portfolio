a453305e44f8cba47ca4d3552ca38a70d542e519
J NITHYASREE <jnakshatraa@gmail.com>
2024-07-21 23:51:53 +0530

    Initial commit with all project files

---
+++ src/app/data.js
@@ -0,0 +1,89 @@
+/*
+Websites:
+
+- https://github.com/pmndrs/gltfjsx (GLTF JSX for 3D Models)
+- https://lucide.dev/icons/ (Lucide Icons)
+- https://github.com/anuraghazra/github-readme-stats (Github Readme Stats)
+- https://skillicons.dev (Skill Icons to show skills)
+- https://github-readme-streak-stats.herokuapp.com (Github Readme Streak Stats)
+
+:root {
+  --background: 27 27 27;
+  --foreground: 225 225 225;
+  --muted: 115 115 115;
+  --accent: 254 254 91; #FEFE5B
+}
+
+*/
+
+export const projectsData = [
+  {
+    id: 1,
+    name: "Enhanced Deepfake Detection",
+    description: "Using EfficientNetB5 & Vgg16",
+    date: "2022-08-15",
+    demoLink: "",
+  },
+  {
+    id: 2,
+    name: "Customer Churn Prediction",
+    description: "Analysing Big Data of Sparkify, Music App",
+    date: "2022-06-20",
+    demoLink: "",
+  },
+  {
+    id: 3,
+    name: "File Segeregation and Unsegregation in Windows",
+    description: "PowerShell script to automate these functions of files within a folder",
+    date: "2022-09-10",
+    demoLink: "",
+  },
+  {
+    id: 4,
+    name: "FreshBasket",
+    description: "E-Commerce Website",
+    date: "2022-05-30",
+    demoLink: "https://healthbeat.example.com",
+  },
+  {
+    id: 5,
+    name: "RailHub",
+    description: "Railway management system",
+    date: "2022-07-12",
+    demoLink: "",
+  },
+  
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://github.com/nithyasreej",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/j-nithyasree/",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "X",
+    link: "https://x.com/j_nithyasr52686",
+    icon: "twitter",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "/J_NITHYASREE_RESUME.pdf",
+    icon: "resume",
+    newTab: true,
+  },
+];
+
+

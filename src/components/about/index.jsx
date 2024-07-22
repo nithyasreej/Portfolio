0a084de2f9d8122895260b924a55510edacfdf23
J Nithyasree <95671947+nithyasreej@users.noreply.github.com>
2024-07-22 12:19:49 +0530

    Update index.jsx

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -5,96 +5,74 @@ import Link from "next/link";
 const AboutDetails = () => {
   return (
     <section className="py-20 w-full">
-      <div className="grid grid-cols-12 gap-4 xs:gap-6  md:gap-8 w-full">
+      <div className="grid grid-cols-12 gap-4 xs:gap-6 md:gap-8 w-full">
         <ItemLayout
-          className={
-            " col-span-full lg:col-span-8 row-span-2 flex-col items-start"
-          }
+          className={"col-span-full lg:col-span-8 row-span-2 flex-col items-start"}
         >
-          <h2 className="  text-xl md:text-2xl text-left w-full capitalize">
-            Architect of Enchantment
+          <h2 className="text-xl md:text-2xl text-left w-full capitalize">
+            Profile
           </h2>
-          <p className="font-light  text-xs sm:text-sm md:text-base   ">
-            My journey in web development is powered by an array of mystical
-            tools and languages, with JavaScript casting the core of my
-            enchantments. I wield frameworks like React.js and Next.js with
-            precision, crafting seamless portals (websites) that connect realms
-            (users) across the digital universe. The ancient arts of the
-            Jamstack empower me to create fast, secure, and dynamic experiences,
-            while my design skills ensure every creation is not only functional
-            but visually captivating. Join me as I continue to explore new
-            spells and technologies to shape the future of the web.
+          <p className="font-light text-xs sm:text-sm md:text-base">
+            Innovative and results-driven Computer Science and Engineering student with a strong foundation in machine
+            learning, deep learning, and software development. Adept at problem-solving and eager to apply skills to real
+            world challenges in a collaborative and fast-paced environment. Proven track record of leading projects and
+            achieving academic excellence.
           </p>
         </ItemLayout>
 
         <ItemLayout
-          className={" col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+          className={"col-span-full lg:col-span-8 row-span-2 flex-col items-start"}
         >
-          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
-            25+ <sub className="font-semibold text-base">clients</sub>
-          </p>
+          <h2 className="text-xl md:text-2xl text-left w-full capitalize">
+            Professional Experience
+          </h2>
+
+          <div className="mb-4">
+            <h3 className="text-lg md:text-xl text-left w-full capitalize">
+              Research Intern
+            </h3>
+            <p className="text-sm md:text-base">
+              June 2024 – Present
+              <br />
+              College Of Engineering Guindy, Anna University Chennai, India
+              <br />
+              • Conducting advanced research on deepfake detection using vision transformers on facial organs under Head of the Department of Computer Science and Engineering, Dr. V. Mary Anita Rajam.
+              <br />
+              • Developed novel techniques to improve deepfake detection accuracy through innovative approaches and cutting-edge technology.
+            </p>
+          </div>
+
+          <div>
+            <h3 className="text-lg md:text-xl text-left w-full capitalize">
+              SDE Intern
+            </h3>
+            <p className="text-sm md:text-base">
+              July 2024 – Present
+              <br />
+              GrapplTech Remote
+              <br />
+              • Assisting in the Frontend development of a scalable Hero-section in the web application using HTML, CSS and React Js.
+              <br />
+              • Collaborated with cross-functional teams to gather requirements and ensure timely delivery of features.
+            </p>
+          </div>
         </ItemLayout>
 
         <ItemLayout
-          className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+          className={"col-span-full lg:col-span-8 row-span-2 flex-col items-start"}
         >
-          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
-            4+{" "}
-            <sub className="font-semibold text-base">years of experience</sub>
+          <h2 className="text-xl md:text-2xl text-left w-full capitalize">
+            Skills
+          </h2>
+          <p className="font-light text-xs sm:text-sm md:text-base">
+            Programming Languages: C/C++, Java, Python, SQL, HTML, CSS, JavaScript, React JS
+            <br />
+            Technologies: React, Deep Learning, Machine Learning, Data Structures and Algorithms, Computer Vision
+            <br />
+            Tools: TensorFlow, Keras, PyTorch, OpenCV, Git, PowerShell, MySQL
+            <br />
+            Soft Skills: Leadership, Communication, Team Collaboration, Problem Solving
           </p>
-        </ItemLayout>
-
-        <ItemLayout
-          className={"col-span-full sm:col-span-6 md:col-span-4 !p-0"}
-        >
-          <img
-            className="w-full h-auto"
-            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/top-langs?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
-            alt="CodeBucks"
-            loading="lazy"
-          />
-        </ItemLayout>
-
-        <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
-          <img
-            className="w-full h-auto"
-            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api?username=codebucks27&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
-            alt="CodeBucks"
-            loading="lazy"
-          />
-        </ItemLayout>
-
-        <ItemLayout className={"col-span-full"}>
-          <img
-            className="w-full h-auto"
-            src={`https://skillicons.dev/icons?i=appwrite,aws,babel,bootstrap,cloudflare,css,d3,docker,figma,firebase,gatsby,git,github,graphql,html,ipfs,js,jquery,kubernetes,linux,mongodb,mysql,netlify,nextjs,nodejs,npm,postgres,react,redux,replit,sass,supabase,tailwind,threejs,vercel,vite,vscode,yarn`}
-            alt="CodeBucks"
-            loading="lazy"
-          />
-        </ItemLayout>
-
-        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
-          <img
-            className="w-full h-auto"
-            src={`${process.env.NEXT_PUBLIC_GITHUB_STREAK_STATS_URL}?user=codebucks27&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B`}
-            alt="CodeBucks"
-            loading="lazy"
-          />
-        </ItemLayout>
-
-        <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
-          <Link
-            href="https://github.com/codebucks27/Nextjs-contentlayer-blog"
-            target="_blank"
-            className="w-full"
-          >
-            <img
-              className="w-full h-auto"
-              src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/pin/?username=codebucks27&repo=Nextjs-contentlayer-blog&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false&description_lines_count=2`}
-              alt="CodeBucks"
-              loading="lazy"
-            />
-          </Link>
         </ItemLayout>
       </div>
     </section>
